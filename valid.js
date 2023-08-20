8ee208cccecc5ea45bc060c8dd7c2ae9d3f67325
EmorBeeHech <137186400+EmorBeehech@users.noreply.github.com>
2023-08-20 10:06:57 +0530

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost/blogApp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const BlogEntry = mongoose.model('BlogEntry', {
+  title: String,
+  content: String,
+  timestamp: { type: Date, default: Date.now }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Simulate a database to store blog entries
-const blogEntries = [];
+app.use(express.static('public')); // Serve static files from the 'public' folder
 
-// Endpoint to submit a new blog entry
-app.post('/submit', (req, res) => {
-  const { title, content } = req.body;
-  blogEntries.push({ title, content });
+// A simple password for demonstration purposes
+const correctPassword = 'securepassword';
+
+app.post('/submit', async (req, res) => {
+  const { password, title, content } = req.body;
+
+  if (password !== correctPassword) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const newEntry = new BlogEntry({ title, content });
+  await newEntry.save();
   res.status(201).json({ message: 'Blog entry submitted successfully.' });
+});
+
+app.get('/get-blogs', async (req, res) => {
+  const entries = await BlogEntry.find().sort('-timestamp');
+  res.json(entries);
 });
 
 app.listen(PORT, () => {
